9443ebc3d09f22eb5e71a7ae4bd5bd91ba08c1f5
Phil Chapman <Phil.Chapman@cruk.manchester.ac.uk>
2017-06-01 14:22:09 +0100

    v0.1.5: Continue with overview vignette

---
+++ R/pgxsim_documentation.R
@@ -0,0 +1,16 @@
+#' @title Simulate Pharmacogenomics Data
+#'
+#' @description
+#'
+#' Package to simulate Pharmacogenomics dose response data
+#'
+#' @details
+#'
+#' To open the vignette type \code{vignette("Overview_of_pgxsim")}
+#'
+#'
+#' @docType package
+#' @name pgxsim-package
+#' @aliases pgxsim
+#' @author Phil Chapman <phil.chapman@cruk.manchester.ac.uk>
+NULL
