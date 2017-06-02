7228421410d29d67745543dd622b4a6a57b9794a
Phil Chapman <Phil.Chapman@cruk.manchester.ac.uk>
2017-06-02 07:13:07 +0100

    v0.1.7: add nlme_gene_fit function and vignette section

---
+++ R/nlme_gene_fit.R
@@ -0,0 +1,20 @@
+#' Mixed Effects Gene Model Fit
+#'
+#' Fit a dose response curve using \pkg{nlme} and including the genetic effect as a covariate
+#'
+#' @param df A data frame with column names resp, conc, gene and cell_id.
+#'
+#' @return an object of class `nlme` representing the nonlinear mixed-effects model fit.
+#' @export
+#'
+#' @examples
+#' #
+nlme_gene_fit <- function(df) {
+  df <- nlme::groupedData(resp~conc|cell_id,data=df)
+  nlme::nlme(resp~1-conc/(exp(ic50 + b*gene)+conc),
+       fixed = ic50+b~1, random = nlme::pdDiag(ic50 ~1),
+       data = df, start = c(1,1), method='ML',
+       verbose = FALSE, control = nlme::nlmeControl(pnlsMaxIter=10,tolerance=1e0)
+  )
+
+}
