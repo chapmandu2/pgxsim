360d30e33e5638080c4b36a530d16eced4240508
Phil Chapman <Phil.Chapman@cruk.manchester.ac.uk>
2017-06-13 12:48:13 +0100

    v0.2.2: Added continuous variable simulation

--- R/sim_cell_lines.R
+++ R/sim_cell_lines.R
@@ -1,10 +1,16 @@
 #' Simulate Cell Lines
 #'
-#' Simulate a cell lines with predefined genetic characteristics
+#' Simulate cell lines with predefined genetic and drug response characteristics.
 #'
 #' @param n Number of cell lines to simulate
 #' @param type Whether genetic covariate is a discrete (d) or continuous (c) variable
 #' @param prop Proportion of cell lines with a discrete characteristic
+#' @param mu mean of the truncated normal distribution
+#' @param sd standard deviation of the truncated normal distribution
+#' @param beta value of the genetic coefficient to recover - note that this is on a log scale
+#' @param pr value of correlation coefficient (0-1 for continuous, ignored for discrete)
+#' @param lb lower bound of the distribution
+#' @param ub upper bound of the distribution
 #' @param sd_prop Cell line specific proportional dose response variance
 #' @param sd_add Cell line specific additive dose response variance
 #'
@@ -14,22 +20,33 @@
 #' @examples
 #' library(dplyr)
 #' set.seed(10000)
-#' sim_cell_lines(n=10, type='d', prop=0.2)
-#' sim_cell_lines(n=10, type='c')
-sim_cell_lines <- function(n, type='d', prop=0.2, sd_prop=0, sd_add=0) {
+#' sim_cell_lines(n=10, type='d', prop=0.2, mu=1, sd=0.1, beta=1)
+#' sim_cell_lines(n=10, type='c', mu=1, sd=0.1, beta=1, pr=0.6)
+sim_cell_lines <- function(n, type='d', prop=0.2, mu=1, sd=0.1, beta=1, pr=0.9, lb=-1.5, ub=Inf, sd_prop=0, sd_add=0) {
 
   if(grepl(paste0('^',type),'discrete')) {
+
     gene <- rep(0,n)
     idx <- sample(seq_len(n), prop*n)
     gene[idx] <- 1
+    pIC50 <- sim_pIC50(mu, sd, beta, g=gene, n=n, type='d', lb=lb, ub=ub)
+
   } else if (grepl(paste0('^',type),'continuous')) {
-    gene <- stats::rnorm(n)
+
+    sigma <- matrix(c(sd^2,pr*(sd*1),
+                      pr*(sd*1),1^2), ncol=2)
+    res <- mvtnorm::rmvnorm(n=n, mean=c(mu,0), sigma=sigma)
+    gene <- res[,2]
+    pIC50 <- res[,1]
+
   } else {
+
     stop('Type must be d (discrete) or c (continuous)')
   }
 
   dplyr::data_frame(cell_id=1:n,
                     gene=gene,
+                    pIC50=pIC50,
                     cl_sd_prop=sd_prop,
                     cl_sd_add=sd_add)
 }
